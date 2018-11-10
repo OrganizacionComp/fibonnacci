f5e62d2b2c72855fd303b0f719805169710c77c7
daledeleo <danny@Danny.localdomain>
2018-11-10 11:58:54 -0500

    Cambios, quite suma y le puse arr[i], directamente

--- fibonacciConArray.cpp
+++ fibonacciConArray.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(int *, int);
+long fibonacci(long *, int);
 
-int fibonacci(int *arr, int fin){
+long fibonacci(long *arr, int fin){
 	int suma = 0;
 	arr[0] = 0;
 	arr[1] = 1;
@@ -15,9 +15,7 @@ int fibonacci(int *arr, int fin){
 	}
 	else{
 		for(int i = 2; i < fin; i++){
-			suma = arr[i-1]+arr[i-2];
-			arr[i]= suma;
-			suma = 0;			
+			arr[i] =arr[i-1]+arr[i-2];
 		}
 	}
 	return arr[fin-1];
@@ -28,11 +26,11 @@ int main(){
 	int fin;
 	printf("Ingrese numero: ");
 	scanf("%d",&fin);
-	fin = fin + 1;
-	int *arr = (int *)malloc(sizeof(int));
-	int n = fibonacci(arr,fin);
+	fin = fin;
+	long arr[fin];
+	long n = fibonacci(arr,fin);
 	for(int i = 0; i < fin ;i++){
-		printf("%4d ------> %4d \n",i,arr[i]);		
+		printf("%d ------> %ld \n",i+1,arr[i]);		
 	}
 	return 1;
 }
